71e8f023e6ad111c65f26620804b7a79f65954fd
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2019-10-15 23:04:36 +0200

    add simple sprite that can move to the right

---
+++ KinematicBody2D.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		move_and_slide(Vector2(100, 0))
+	
