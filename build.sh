90aa76427ed55a079286bed082502baf33ac8f76
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2019-11-01 00:34:11 +0100

    adds build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+
+wget https://downloads.tuxfamily.org/godotengine/3.1.1/Godot_v3.1.1-stable_export_templates.tpz
+wget https://downloads.tuxfamily.org/godotengine/3.1.1/Godot_v3.1.1-stable_linux_headless.64.zip
