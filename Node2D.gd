d20eae39014e97ba69b9f657b726c2248c4698b6
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-03-23 18:33:52 +0100

    2d test

---
+++ Node2D.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	for x in range(10):
+		for y in range(10):
+			pass #$TileMap.set_cell(x, y, randi() % 2 - 1)
+	
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var button = event as InputEventMouseButton
+		if button.pressed:
+			pass
+			#$TileMap.set_cell(int(button.position.x / 8), int(button.position.y / 8), -1)
+			
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func place_material(position, material):
+	var mat_scene = preload("res://GenericMaterial.tscn")
+	var node = mat_scene.instance()
+	node.translate(position)
+	add_child(node)
+
+
+func _on_KinematicBody2D_mined_at(position):
+	print("player mined ad ", position)
+	var map_point = $TileMap.world_to_map(position)
+	
+	var material = $TileMap.get_cellv(map_point)
+	print(material)
+	place_material(position, material)
+	$TileMap.set_cellv(map_point, -1)
+	
+	pass # Replace with function body.
